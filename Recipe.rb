215d3f40f93b9530fa4dd9be183236c4d62bce76
Gladys Toledo-Rodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2025-01-24 08:28:19 +0000

    Fixed format of detail recipe view

--- Recipe.rb
+++ Recipe.rb
@@ -7,10 +7,16 @@ class Recipe
   end
 
   def recipe_card
-    card = "
-    Name: #{@name}
-    \nIngredients: #{@ingredients.join(', ')}
-    \nInstructions: #{@instructions.join(', ')}
+    ingredients_string = ""
+    @ingredients.each do |item|
+      ingredients_string = ingredients_string + "\n#{item}"
+    end
+    instructions_string = ""
+    @instructions.each_with_index do |item, index|
+      instructions_string = instructions_string+ "\n#{index+1}. #{item}"
+    end
+
+    card = "\nName: #{@name}\n\nIngredients: #{ingredients_string}\n\nInstructions: #{instructions_string}
     "
     return card
   end
