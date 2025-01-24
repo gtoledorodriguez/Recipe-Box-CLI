841ca1579804f1de1d8ae436cf39837ba551f765
Gladys Toledo-Rodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2025-01-24 07:37:39 +0000

    added functions

--- RecipeBoxCLI.rb
+++ RecipeBoxCLI.rb
@@ -1,4 +1,19 @@
 require "./Recipe.rb"
 
 class RecipeBoxCLI
+  attr_accessor :recipes #List of Recipe Objects
+  def initialize
+    @recipes = []
+  end
+
+  #Main Menu - Everything runs here
+  #Add Recipes
+  def add_recipe
+  end
+  #View Recipes
+  def view_recipes
+  end
+  #Edit Recipes
+  def edit_recipe
+  end
 end
